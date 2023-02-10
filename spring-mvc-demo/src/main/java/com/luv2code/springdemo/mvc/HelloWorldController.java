15e7a8f88d2e26f1235557d0eacadd7127f9e564
Sem Zhivotov <semzhivotov@gmail.com>
2023-02-10 15:47:02 +0200

    spring MVC Request Mapping

--- spring-mvc-demo/src/main/java/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ spring-mvc-demo/src/main/java/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -37,6 +38,21 @@ public class HelloWorldController {
 
         // create the message
         String result ="Yo! " + theName;
+
+        // add message to the model
+        model.addAttribute("message", result);
+
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String theName, Model model){
+
+        // convert the data to all caps
+        theName = theName.toUpperCase();
+
+        // create the message
+        String result ="Hey My Friend from v3 " + theName;
 
         // add message to the model
         model.addAttribute("message", result);
--- spring-mvc-demo/src/main/webapp/WEB-INF/view/helloworld-form.jsp
+++ spring-mvc-demo/src/main/webapp/WEB-INF/view/helloworld-form.jsp
@@ -12,7 +12,7 @@
 </head>
 <body>
 
-    <form action="processFormVersionTwo" method="get">
+    <form action="processFormVersionThree" method="get">
         <input type="text" name="studentName"
             placeholder="What's your name?"/>
 
