038e6a5fc90ee5b71855174a2546ad57ace9c55a
Sem Zhivotov <semzhivotov@gmail.com>
2023-02-26 21:23:20 +0200

    hibernate (ORM) - Advanced mapping one-to-one/many

---
+++ hb-one-to-many/src/main/java/com/luv2code/hibernate/demo/CreateInstructorDemo.java
@@ -0,0 +1,60 @@
+package com.luv2code.hibernate.demo;
+
+import com.luv2code.hibernate.demo.entity.Course;
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class CreateInstructorDemo {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Course.class)
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			
+			Instructor tempInstructor = 
+					new Instructor("Abdul", "Husane", "abdul@gmail.com");
+			
+			InstructorDetail tempInstructorDetail =
+					new InstructorDetail(
+							"http://www.youtube.com",
+							"BigBadaBoom");
+			
+			// associate the objects
+			tempInstructor.setInstructorDetail(tempInstructorDetail);
+			
+			// start a transaction
+			session.beginTransaction();
+
+			System.out.println("Saving instructor: " + tempInstructor);
+			session.save(tempInstructor);					
+			
+			// commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+		}
+		finally {
+			session.close();
+			factory.close();
+		}
+	}
+
+}
+
+
+
+
+
---
+++ hb-one-to-many/src/main/java/com/luv2code/hibernate/demo/entity/InstructorDetail.java
@@ -0,0 +1,90 @@
+package com.luv2code.hibernate.demo.entity;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name="instructor_detail")
+public class InstructorDetail {
+
+	// annotate the class as an entity and map to db table
+	
+	// define the fields
+	
+	// annotate the fields with db column names
+	
+	// create constructors
+	
+	// generate getter/setter methods
+	
+	// generate toString() method
+
+	@OneToOne(mappedBy = "instructorDetail", cascade = CascadeType.ALL)
+	private Instructor instructor;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="youtube_channel")
+	private String youtubeChannel;
+	
+	@Column(name="hobby")
+	private String hobby;
+
+	
+	public InstructorDetail() {
+		
+	}
+
+	public InstructorDetail(String youtubeChannel, String hobby) {
+		this.youtubeChannel = youtubeChannel;
+		this.hobby = hobby;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Instructor getInstructor() {
+		return instructor;
+	}
+
+	public void setInstructor(Instructor instructor) {
+		this.instructor = instructor;
+	}
+
+	public String getYoutubeChannel() {
+		return youtubeChannel;
+	}
+
+	public void setYoutubeChannel(String youtubeChannel) {
+		this.youtubeChannel = youtubeChannel;
+	}
+
+	public String getHobby() {
+		return hobby;
+	}
+
+	public void setHobby(String hobby) {
+		this.hobby = hobby;
+	}
+
+	@Override
+	public String toString() {
+		return "InstructorDetail [id=" + id + ", youtubeChannel=" + youtubeChannel + ", hobby=" + hobby + "]";
+	}
+		
+}
+
+
+
+
+
+
+
