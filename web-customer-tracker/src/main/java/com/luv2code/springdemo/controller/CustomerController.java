cf24db57275b27f280c6a65ee2e323de7e936902
Sem Zhivotov <semzhivotov@gmail.com>
2023-02-28 19:41:57 +0200

    Database Web App - Spring MVC and Hibernate Project - starting

---
+++ web-customer-tracker/src/main/java/com/luv2code/springdemo/controller/CustomerController.java
@@ -0,0 +1,18 @@
+package com.luv2code.springdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+
+    @RequestMapping("/list")
+    public String listCustomer(Model theModel){
+
+        return "list-customers";
+
+    }
+
+}
---
+++ web-customer-tracker/src/main/webapp/WEB-INF/view/list-customers.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>List Customers</title>
+</head>
+<body>
+
+List Customers - coming soon..
+
+</body>
+</html>
