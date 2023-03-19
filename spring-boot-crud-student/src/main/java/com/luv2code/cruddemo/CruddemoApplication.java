d0506961a67335bbfbfdd56468efcd6a7a63dfb8
Sem Zhivotov <semzhivotov@gmail.com>
2023-03-19 16:09:09 +0200

    spring boot - database crud student

---
+++ spring-boot-crud-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -0,0 +1,22 @@
+package com.luv2code.cruddemo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CruddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(){
+		return runner->{
+			System.out.println("Hello World");
+		};
+	}
+
+}
