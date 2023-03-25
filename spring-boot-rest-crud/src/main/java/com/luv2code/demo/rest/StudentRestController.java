f79fa05f1daf66d508793c2e73afeb47a701caab
Sem Zhivotov <semzhivotov@gmail.com>
2023-03-25 23:59:20 +0200

    spring boot - rest crud ExceptionHandler, ControllerAdvice

--- spring-boot-rest-crud/src/main/java/com/luv2code/demo/rest/StudentRestController.java
+++ spring-boot-rest-crud/src/main/java/com/luv2code/demo/rest/StudentRestController.java
@@ -36,16 +36,4 @@ public class StudentRestController {
         return students.get(studentId);
     }
 
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException (StudentNotFoundException exc){
-
-        StudentErrorResponse error = new StudentErrorResponse();
-
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-    }
-
 }
