dbf84fae685420a5e5f5479cc6a283bf2ae3bba0
Sem Zhivotov <semzhivotov@gmail.com>
2023-04-08 17:28:00 +0300

    spring boot - spring MVC with thymeleaf

---
+++ thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/DemoController.java
@@ -0,0 +1,19 @@
+package com.luv2code.springboot.thymeleafdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+
+    @GetMapping("/hello")
+    public String sayHello(Model model){
+
+        model.addAttribute("data", new java.util.Date());
+
+        return "helloworld";
+
+    }
+
+}
