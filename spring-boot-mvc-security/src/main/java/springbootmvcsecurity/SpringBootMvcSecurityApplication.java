a2741202a01a9824201797ea76e46540d0946a94
Sem Zhivotov <semzhivotov@gmail.com>
2023-06-25 02:01:52 +0300

    spring - mvc - security start

---
+++ spring-boot-mvc-security/src/main/java/springbootmvcsecurity/SpringBootMvcSecurityApplication.java
@@ -0,0 +1,13 @@
+package springbootmvcsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMvcSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMvcSecurityApplication.class, args);
+    }
+
+}
